ba68b2a6acf40259428770169417e6405ec0277e
Vaibhav Gupta <vaibhav.gupta@sophos.com>
2023-02-21 21:56:20 -0500

    added healthz, metrics and logging

--- techtrends/app.py
+++ techtrends/app.py
@@ -1,4 +1,6 @@
+import logging
 import sqlite3
+
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
@@ -18,6 +20,20 @@ def get_post(post_id):
     connection.close()
     return post
 
+# Function to get connections count from number of posts
+def retrieve_metrics_from_articles_count(metrics_object):
+    """
+    Count the number of news articles and increment the number of connections used for them
+    Parameters:
+    metrics_object (dict): Dictionary with basic data for metrics endpoint response
+    """
+    connection = get_db_connection()
+    article_count = connection.execute('SELECT count(*) FROM posts').fetchone()
+    connection.close()
+
+    metrics_object['db_connection_count'] += 1
+    metrics_object['post_count'] = article_count[0]    
+
 # Define the Flask application
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
@@ -36,13 +52,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      logging.error('Article {} does not exists!'.format(post_id))  
       return render_template('404.html'), 404
     else:
+      logging.info('Article "{}" retrieved!'.format(post['title']))  
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    logging.info('"About Us" page is retrieved')
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -65,6 +84,34 @@ def create():
 
     return render_template('create.html')
 
+# Define health check functionality
+@app.route('/healthz', methods=['GET'])
+def healthz():
+    response = app.response_class(
+        response=json.dumps({'result': 'OK - healthy'}),
+        status=200,
+        mimetype='application/json')
+
+    return response
+
+# Define metrics functionality
+@app.route('/metrics', methods=['GET'])
+def metrics():
+    metrics_object = {
+        'db_connection_count': 0,
+        'posts_count': None
+    }
+
+    retrieve_metrics_from_articles_count(metrics_object)
+
+    response = app.response_class(
+        response=json.dumps(metrics_object),
+        status=200,
+        mimetype='application/json')
+
+    return response
+
 # start the application on port 3111
 if __name__ == "__main__":
+   logging.basicConfig(level=logging.DEBUG)
    app.run(host='0.0.0.0', port='3111')
